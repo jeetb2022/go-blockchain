b8b32a696629475b1e0fcb5ede42d2efee923adb
Jeet <jeetbhadaniya1228@gmail.com>
2024-05-05 16:35:38 +0530

    feat: Added multi-connect functionality with peers

--- api/api.go
+++ api/api.go
@@ -2,29 +2,58 @@ package api
 
 import (
 	"Blockchain_Project/database"
+	"Blockchain_Project/network"
 	"Blockchain_Project/transaction"
+	"Blockchain_Project/txpool"
+	"bytes"
+	"crypto/rand"
 	"fmt"
+	"math/big"
+	math_rand "math/rand"
 	"net/http"
+
+	"github.com/ethereum/go-ethereum/common"
 )
+
+var tp *txpool.TransactionPool
+
+func GenerateRandomAddress() common.Address {
+	// Generate a random byte slice of length 20 (address size)
+	randomBytes := make([]byte, 20)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// Convert the byte slice to a common.Address
+	randomAddress := common.BytesToAddress(randomBytes)
+	return randomAddress
+}
 
 // Handler function to handle /sendTx endpoint
 func SendTxHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse query parameter "signed" to get the signed transaction
-	signedTx := r.URL.Query().Get("signed")
-
-	// Decode the signed transaction from base64 or hex or any other encoding if needed
-
-	// Deserialize the signed transaction (RLP decoding)
-	var tx transaction.Transaction
-	fmt.Println(tx)
-	fmt.Println(signedTx)
-	// Decode the signed transaction into the tx struct
-
-	// Process the transaction (e.g., add it to the transaction pool, validate, etc.)
-
-	// Send response
+	tx := transaction.SignedTransaction{
+		Nonce: 43,
+		To:    GenerateRandomAddress(),
+		Value: 1000,
+		V:     big.NewInt(int64(math_rand.Intn(1000))),
+		R:     big.NewInt(int64(math_rand.Intn(1000))),
+		S:     big.NewInt(int64(math_rand.Intn(1000))),
+	}
+	if !transaction.VerifyTx(tx) {
+		http.Error(w, "Transaction verification failed", http.StatusBadRequest)
+	}
+	tp.AddTransactionToTxPool(&tx)
+	// tp.GetAllTransactions()
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Transaction received and processed successfully")
+	message := "Transaction added scessfully"
+	_, err := w.Write([]byte(message))
+	if err != nil {
+		// Handle error if unable to write to response
+		panic(err)
+	}
+
 }
 
 // Handler function to handle /blockNumber endpoint
@@ -39,8 +68,6 @@ func BlockNumberHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Recent most block number: %d", recentBlockNumber)
 }
-
-
 
 // // Handler function to handle /block endpoint with query parameter "hash"
 // func blockByHashHandler(w http.ResponseWriter, r *http.Request) {
@@ -64,22 +91,26 @@ func BlockNumberHandler(w http.ResponseWriter, r *http.Request) {
 // 	w.Write(blockJSON)
 // }
 
-
 // // Handler function to handle /tx endpoint with query parameter "hash"
 // func txHandler(w http.ResponseWriter, r *http.Request) {
 // 	// Parse query parameter "hash" to get the transaction hash
-// 	txHash := r.URL.Query().Get("hash")
-
-// 	// Get the transaction with the specified transaction hash from the blockchain
-
-// 	// Serialize the transaction into JSON
-// 	txJSON, _ := json.Marshal(transaction)
-
-// 	// Send response with the transaction JSON
-// 	w.Header().Set("Content-Type", "application/json")
+// 	tx := transaction.SignedTransaction{}
+// 	if !transaction.VerifyTx(tx) {
+// 		http.Error(w, "Transaction verification failed", http.StatusBadRequest)
+// 	}
+// 	tp.AddTransactionToTxPool(&tx)
 // 	w.WriteHeader(http.StatusOK)
-// 	w.Write(txJSON)
+// 	message := "Transaction added sucessfully"
+// 	_, err := w.Write([]byte(message))
+// 	if err != nil {
+// 		// Handle error if unable to write to response
+// 		panic(err)
+// 	}
+
 // }
+func GetTxPool(p *txpool.TransactionPool) {
+	tp = p
+}
 
 // Handler function to handle /getNonce endpoint with query parameter "address"
 func GetNonceHandler(w http.ResponseWriter, r *http.Request) {
@@ -103,7 +134,6 @@ func GetNonceHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Nonce of account %s: %d", addressString, nonce)
 }
-
 
 // Handler function to handle /getBalance endpoint with query parameter "address"
 func GetBalanceHandler(w http.ResponseWriter, r *http.Request) {
@@ -130,17 +160,35 @@ func GetBalanceHandler(w http.ResponseWriter, r *http.Request) {
 
 // Handler function to handle /getknownhost endpoint
 func GetKnownHostHandler(w http.ResponseWriter, r *http.Request) {
+	
 	// Parse query parameter "address" to get the account address
-	address := r.URL.Query().Get("address")
-
-	// Your implementation to check if the address is known goes here
-	if address == "0x1234567890" {
-		// Send response
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Address %s is known", address)
-		return
+	addrList := network.GetPeerAddrs()
+	var buf bytes.Buffer
+	for _, addr := range addrList {
+		buf.WriteString(addr)
+		buf.WriteByte('\n') // Add a newline separator
 	}
-	// Send response
+
+	// Convert the buffer to a byte slice
+	addrBytes := buf.Bytes()
+
+	// message := "Transaction added scessfully"
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Address %s is known or unknown", address)
+	_, err := w.Write([]byte(addrBytes))
+	if err != nil {
+		// Handle error if unable to write to response
+		panic(err)
+	}
+
+	// address := r.URL.Query().Get("address")
+
+	// // Your implementation to check if the address is known goes here
+	// if address == "0x1234567890" {
+	// 	// Send response
+	// 	w.WriteHeader(http.StatusOK)
+	// 	fmt.Fprintf(w, "Address %s is known", address)
+	// 	return
+	// }
+	// Send response
+	// fmt.Fprintf(w, "Address %s is known or unknown", address)
 }
--- txpool/txpool.go
+++ txpool/txpool.go
@@ -15,43 +15,27 @@ import (
 )
 
 type TransactionPool struct {
-	Transactions []*transaction.SignedTransaction
+	transactions []*transaction.SignedTransaction
+	newTxChan    chan *transaction.SignedTransaction
 }
 
-// NewTransactionPool creates a new transaction pool
 func NewTransactionPool() *TransactionPool {
 	return &TransactionPool{
-		Transactions: make([]*transaction.SignedTransaction, 0),
+		transactions: make([]*transaction.SignedTransaction, 0),
+		newTxChan:    make(chan *transaction.SignedTransaction),
 	}
 }
 
-// AddTransaction adds a new transaction to the pool after validation
-func (tp *TransactionPool) AddTransactionToTxPool(tx *transaction.SignedTransaction) error {
-	// Basic validation
-	// if tx == nil {
-	// 	return errors.New("transaction is nil")
-	// }
-	// if tx.To == (common.Address{}) {
-	// 	return errors.New("invalid recipient address")
-	// }
-	// if tx.Value == 0 {
-	// 	return errors.New("invalid transaction value")
-	// }
-	// if tx.Nonce == 0 {
-	// 	return errors.New("invalid transaction nonce")
-	// }
-	// if tx.V == nil || tx.R == nil || tx.S == nil {
-	// 	return errors.New("signature components missing")
-	// }
-	// Add to pool
-	tp.Transactions = append(tp.Transactions, tx)
-	return nil
+func (tp *TransactionPool) AddTransactionToTxPool(tx *transaction.SignedTransaction) {
+	tp.transactions = append(tp.transactions, tx)
+	// Send the new transaction through the channel
+	tp.newTxChan <- tx
 }
 
 // PrintAllTransactions prints all transactions in the pool
 func (tp *TransactionPool) GetAllTransactions() {
 	fmt.Println("Transactions in the pool:")
-	for _, tx := range tp.Transactions {
+	for _, tx := range tp.transactions {
 		fmt.Printf("To: %s\n", tx.To.Hex())
 		fmt.Printf("Value: %d\n", tx.Value)
 		fmt.Printf("Nonce: %d\n", tx.Nonce)
