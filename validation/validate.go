35189bb83bc21182da4ed9438efa06c3bb1e8a0d
neel <neelxdxd@gmail.com>
2024-05-05 16:36:42 +0530

    Block validated & messages functions created but not tested

--- validation/validate.go
+++ validation/validate.go
@@ -3,9 +3,17 @@ package validation
 import (
 	"Blockchain_Project/blockchain"
 	"Blockchain_Project/database"
+	"Blockchain_Project/transaction"
 	"Blockchain_Project/utils"
 	"errors"
 	"fmt"
+	"math/big"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+	"golang.org/x/crypto/sha3"
 )
 
 func ValidateAddress(address [20]byte) (bool, error) {
@@ -55,4 +63,91 @@ func ValidateBlock(block *blockchain.Block) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func rlpHash(x interface{}) (h common.Hash) {
+	sha := hasherPool.Get().(crypto.KeccakState)
+	defer hasherPool.Put(sha)
+	sha.Reset()
+	rlp.Encode(sha, x)
+	sha.Read(h[:])
+
+	return h
+}
+
+var hasherPool = sync.Pool{
+	New: func() interface{} { return sha3.NewLegacyKeccak256() },
+}
+
+func Hash(tx *transaction.Transaction) common.Hash {
+	return rlpHash([]interface{}{
+		tx.To,
+		tx.Value,
+		tx.Nonce,
+	})
+}
+
+func recoverPlain(sighash common.Hash, R, S, Vb *big.Int, homestead bool) (common.Address, error) {
+	if Vb.BitLen() > 8 {
+		// return common.Address{}, ErrInvalidSig
+		panic("invalid signature")
+	}
+	V := byte(Vb.Uint64() - 27)
+	if !crypto.ValidateSignatureValues(V, R, S, homestead) {
+		// return common.Address{}, ErrInvalidSig
+		panic("invalid signature")
+	}
+	// encode the signature in uncompressed format
+	r, s := R.Bytes(), S.Bytes()
+	sig := make([]byte, crypto.SignatureLength)
+	copy(sig[32-len(r):32], r)
+	copy(sig[64-len(s):64], s)
+	sig[64] = V
+	// recover the public key from the signature
+	pub, err := crypto.Ecrecover(sighash[:], sig)
+	if err != nil {
+		return common.Address{}, err
+	}
+	if len(pub) == 0 || pub[0] != 4 {
+		return common.Address{}, errors.New("invalid public key")
+	}
+	var addr common.Address
+	copy(addr[:], crypto.Keccak256(pub[1:])[12:])
+	return addr, nil
+}
+
+func ValidateTransaction(trans *transaction.SignedTransaction) bool {
+	tx := &transaction.Transaction{
+		To:    trans.To,
+		Value: trans.Value,
+		Nonce: trans.Nonce,
+	}
+	sender, err := recoverPlain(Hash(tx), trans.R, trans.S, trans.V, true)
+	if err != nil {
+		return false
+	}
+	// Check if the sender's account exists in the database
+	senderAccount, err := database.GetAccountFromDB(sender)
+	if err != nil {
+		fmt.Println("Error retrieving sender's account from database:", err)
+		return false
+	}
+
+	// Check if the sender's account has sufficient balance
+	if senderAccount.Balance < tx.Value {
+		fmt.Println("Insufficient balance in sender's account")
+		return false
+	}
+
+	return true
+}
+
+func decodeSignature(sig []byte) (r, s, v *big.Int) {
+	if len(sig) != crypto.SignatureLength {
+		panic(fmt.Sprintf("wrong size for signature: got %d, want %d", len(sig), crypto.SignatureLength))
+	}
+	r = new(big.Int).SetBytes(sig[:32])
+	s = new(big.Int).SetBytes(sig[32:64])
+	v = new(big.Int).SetBytes([]byte{sig[64] + 27})
+	return r, s, v
 }
